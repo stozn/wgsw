91ca32a8493e67a5de7709a9456a59333bd5ab1f
stozn <stozn.cn@outlook.com>
2024-08-30 12:48:54 +0800

    更新：模仿棋

--- ai/模仿棋.py
+++ ai/模仿棋.py
@@ -2,7 +2,7 @@
 
 from api.get import *
 import math
-
+import realm
 
 def update(context):
     bots = my_bots()  # 获取当前所有机器人列表
@@ -14,24 +14,24 @@ def update(context):
     war = my_bot_by_type('warrior')
     pro = my_bot_by_type('protector')
     arc = my_bot_by_type('archer')
+    notatk = {'warrior': True,
+                'protector': True,
+                'archer': True
+                }
 
     act = True  # 标记是否已经确定行动，ture为未决策完，不用return会灵活一些，可以根据后面的情况变更行动方案
-    mofang =True # 是否已经进攻，若攻击则不再模仿棋，改为抢补给同款策略
     enms = enemy_bots()
     ops = enemy_last_round_ops()
-    bjq = [(3,3), (3,4), (4,3), (4,4)]
 
     # 如果指挥官被攻击，则快速回防
     for enm in enms:
-        bjq = [b for b in bjq if b[0]!=enm.row or b[1]!=enm.col]
         print([bot.type for bot in enm.get_attackable_bots_in_move_range()])
         if 'commander' in [bot.type for bot in enm.get_attackable_bots_in_move_range()]:
             sorted([bot for bot in bots if bot.type != 'commander'],
                    key=lambda bot: bot.distance(enm))[0].to_attack(enm)
             act = False
-            mofang = False
 
-    # 优先执行本轮可以进行的攻击  *** TODO 可以进补给区再攻击要先进补给区
+    # 优先执行本轮可以进行的攻击
     if act:
         atk_tms = 20  # 最低攻击次数
         for bot in bots:
@@ -43,58 +43,36 @@ def update(context):
                 if tms < atk_tms:
                     bot.to_attack(atk_enms[0])  # 平台会有行动覆盖，自动会取最后一次行动
                     act = False  # 行动完成，后面的移动就不执行了
-                    mofang = False
+                    notatk[bot.type]
 
-
-    '''
-    NOTE
-    模仿棋这里主要是第一次主动攻击后就破坏了对称关系，后续的行动策略如何设计？
-    '''
-    if context.round > 95: # 超过95局就停止模仿，5步够战士到达补给区了，先抢到补给拿下比赛
-        mofang = False
-    # 没有可攻击，则模仿棋 *** BUG 检测移动的位置是否有效，因为优先攻击后位置与对手不再2是对称的了
-    if mofang and act and ops and ops[0].move:
+    # 没有可攻击，则模仿棋
+    if act and ops and ops[0].move:
         enm_bot = ops[0].bot
         print(enm_bot.type, ops[0].move)
         dr, dc = ops[0].move
         bot = my_bot_by_type(enm_bot.type)
-        if bot:  # 如果还有这个兵种，就继续模仿
+        if bot and notatk[enm_bot.type]:  # 如果还有这个兵种，就继续模仿
             bot.move_to((bot.row - dr, bot.col - dc))
             act = False  # 行动完成，后面的移动就不执行了
-            mofang = False
-            
-    # 模仿棋状态，但是对方未行动，则等待
-    if mofang and act:
-        act = False  # 行动完成，后面的移动就不执行了
-        mofang = False
 
     # 对方不移动，先抢补给
     def on(bot, pos):
         return bot.row == pos[0] and bot.col == pos[1]
-    
-    # 优先使用战士快速抢占补给区ps：暂时是按进攻方  *** TODO 抢占的位置合理分配（如已经被占领）
-    if act and war:
-        pos = (war.row, war.col)
-        if pos not in bjq:
-            pos = sorted(bjq, key=lambda b: war.distance(b))
-            if pos:
-                war.toward(pos[0])
-            act = False
 
-    # 其次使用弓箭手抢占补给区,提供火力输出
-    if act and arc:
-        pos = (arc.row, arc.col)
-        if pos not in bjq:
-            pos = sorted(bjq, key=lambda b: arc.distance(b))
-            if pos:
-                arc.toward(pos[0])
-            act = False
+    # 优先使用战士快速抢占补给区（3,3）ps：暂时是按进攻方
+    pos = (3, 3)
+    if act and war and not on(war, pos):
+        war.toward(pos)
+        act = False
 
-    # 然后使用守卫抢占补给区
-    if act and pro:
-        pos = (pro.row, pro.col)
-        if pos not in bjq:
-            pos = sorted(bjq, key=lambda b: pro.distance(b))
-            if pos:
-                pro.toward(pos[0])
-            act = False
+    # 其次使用弓箭手抢占补给区（4,3）,提供火力输出
+    pos = (4, 3)
+    if act and arc and not on(arc, pos):
+        arc.toward(pos)
+        act = False
+
+    # 然后使用守卫抢占补给区（4,4）
+    pos = (4, 4)
+    if act and pro and not on(pro, pos):
+        pro.toward(pos)
+        act = False
